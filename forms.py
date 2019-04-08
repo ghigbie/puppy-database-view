6b5ebb824348a4a0633d471ab6166fc1d0818d19
ghigbie <gehigbie@gmail.com>
2019-04-07 18:14:18 -0600

    Add forms to forms.py

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+class AddForm(FlaskForm):
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+    id = IntegerField('ID Number of Puppy who was adoped')
+    submit = SubmitField('Run to Your New Owner')
