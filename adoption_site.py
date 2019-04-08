0f4281862c600b82e4217cf7d156f60b5bfe581b
ghigbie <gehigbie@gmail.com>
2019-04-07 18:03:27 -0600

    Add more app configurations

--- adoption_site.py
+++ adoption_site.py
@@ -3,3 +3,14 @@ import os
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SECREK_KEY'] == 'youwillneverguessme'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///'+ os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
