ba0de7aa8acd3ad5d403b015d654332a8a6bd176
ghigbie <gehigbie@gmail.com>
2019-04-08 21:29:25 -0600

    Add add_owner route

--- adoption_site.py
+++ adoption_site.py
@@ -59,10 +59,14 @@ def index():
 
 @app.route('/add_owner', methods=['GET', 'POST'])
 def addowner():
-    form = AddOwner()
+    form = AddOwnerForm()
     if form.validate_on_submit():
         name = form.name.data
-        puppy = 
+        pup_id = form.pup_id.data
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+        return redirect(url_for('list_pup'))
     return render_template('addowner.html')
 
 @app.route('/add', methods=['GET', 'POST'])
