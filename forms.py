318110d88ea144f2bd42c489e80a59b16eec2463
ghigbie <gehigbie@gmail.com>
2019-04-08 21:23:54 -0600

    Add form for owner

--- forms.py
+++ forms.py
@@ -5,7 +5,10 @@ class AddForm(FlaskForm):
     name = StringField('Name of Puppy: ')
     submit = SubmitField('Add Puppy')
 
-
 class DeleteForm(FlaskForm):
     id = IntegerField('ID Number of Puppy who was adoped: ')
     submit = SubmitField('Run to Your New Owner')
+
+class AddOwner(FlaskForm):
+    name = StringField('Name of Owner: ')
+    submit = SubmitField('Add Owner')
